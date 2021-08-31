903d83461a683d3bbe119400c0d5c8c0966284ed
DavDev <dawtom97@gmail.com>
2021-08-31 17:00:43 +0200

    beta-2

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router(); 
 
-/* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Expressio' });
+  res.render('news', { title: 'News' });
 });
 
 
